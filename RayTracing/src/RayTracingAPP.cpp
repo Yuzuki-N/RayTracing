bc0deb1efa48b3af89b0450ef5068b4c020e32f3
yuzuki <nanami2962@gmail.com>
2022-07-01 11:39:52 +0800

    render a image

---
+++ RayTracing/src/RayTracingAPP.cpp
@@ -0,0 +1,80 @@
+#include "Walnut/Application.h"
+#include "Walnut/EntryPoint.h"
+
+#include "Walnut/Image.h"
+
+using namespace Walnut;
+
+class ExampleLayer : public Walnut::Layer
+{
+public:
+	virtual void OnUIRender() override
+	{
+		ImGui::Begin("Settings");
+		if (ImGui::Button("Render"))
+		{
+			Render();
+		}
+		ImGui::End();
+
+		ImGui::ShowDemoWindow();
+
+		// 消除padding
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 0.0f, 0.0f });
+
+		ImGui::Begin("Viewport");
+
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+		if (m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(),
+				{ (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+
+		ImGui::End();
+		ImGui::PopStyleVar();
+	}
+
+	void Render()
+	{
+		// 图像为空或者图像与viewport尺寸不匹配
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+
+			for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
+			{
+				// ABGR
+				m_ImageData[i] = 0xffff00ff;
+			}
+			m_Image->SetData(m_ImageData);
+		}
+	}
+private:
+	std::shared_ptr<Image> m_Image;
+	uint32_t m_ViewportWidth, m_ViewportHeight;
+	uint32_t* m_ImageData = nullptr;
+};
+
+Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+{
+	Walnut::ApplicationSpecification spec;
+	spec.Name = "Ray Tracing";
+
+	Walnut::Application* app = new Walnut::Application(spec);
+	app->PushLayer<ExampleLayer>();
+	app->SetMenubarCallback([app]()
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Exit"))
+				{
+					app->Close();
+				}
+				ImGui::EndMenu();
+			}
+		});
+	return app;
+}
